6f93ffde0096d7af9d16fc9d338b953298239f8e
niwa_reina <niwa_reina@niwanombookpuro>
2020-05-15 18:23:25 +0900

    あいうえお

---
+++ SE/src/SE_1/Food.java
@@ -0,0 +1,12 @@
+package SE_1;
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("好きな食べ物は餃子です。"+"\n"+"理由は、単純に美味しいし、体に悪そうな味がたまらないからです。");
+		
+	}
+
+}
